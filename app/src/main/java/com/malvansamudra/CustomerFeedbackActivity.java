b0c4729580c435d52ff169368aedd8d4345d50f6
Brijesh Dutta <brijeshdtt@gmail.com>
2017-11-23 17:04:06 +0530

    Initial commit

---
+++ app/src/main/java/com/malvansamudra/CustomerFeedbackActivity.java
@@ -0,0 +1,13 @@
+package com.malvansamudra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomerFeedbackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_feedback);
+    }
+}
